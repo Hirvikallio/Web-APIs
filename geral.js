b7ea3a4af8bd48c9abfabad275d932a83368e28d
Hirvikallio <117479608+Hirvikallio@users.noreply.github.com>
2023-12-25 19:02:32 +0000

    dnd

--- geral.js
+++ geral.js
@@ -1,61 +1,52 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const apiEndpoint = 'https://gutendex.com/books';
-
-    // Function to fetch fox data from the API
-    async function fetchFoxes() {
-        try {
-            const response = await fetch(apiEndpoint);
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Erro ao consultar a API:', error);
-        }
-    }
-
-    // Function to display foxes in the results container
-    function displayFoxes(foxes) {
-        const resultsContainer = document.getElementById('apiResults');
-        resultsContainer.innerHTML = '';
-
-        foxes.forEach(fox => {
-            const resultItem = document.createElement('div');
-            resultItem.classList.add('result-item');
-            resultItem.innerHTML = `<img src="${fox.image}" alt="Fox">`;
-            resultsContainer.appendChild(resultItem);
-        });
-    }
-
-    // Function to perform search, filter, and sort operations
-    async function searchFoxes() {
-        const name = document.getElementById('nameInput').value;
-        const category = document.getElementById('categoryFilter').value;
-
-        // Fetch foxes from the API
-        const foxes = await fetchFoxes();
-
-        // Filter by name
-        const filteredFoxes = name
-            ? foxes.filter(fox => fox.name.toLowerCase().includes(name.toLowerCase()))
-            : foxes;
-
-        // Filter by category
-        const finalFoxes = category
-            ? filteredFoxes.filter(fox => fox.category.toLowerCase() === category.toLowerCase())
-            : filteredFoxes;
-
-        // Display the results
-        displayFoxes(finalFoxes);
-    }
-
-    // Assign the search function to the button click event
-    document.getElementById('apiForm').addEventListener('submit', function (event) {
+document.addEventListener("DOMContentLoaded", function () {
+    // Manipulação de eventos do formulário
+    document.getElementById("searchForm").addEventListener("submit", function (event) {
         event.preventDefault();
-        searchFoxes();
+
+        // Obtenha o valor selecionado do filtro de categoria
+        const selectedCategory = document.getElementById("categoryFilter").value;
+        const sortOrder = document.getElementById("sortOrder").value;
+
+        // Mapeamento de categorias para os endpoints da API D&D 5e
+        const categoryEndpoints = {
+            classes: "https://www.dnd5eapi.co/api/classes",
+            spells: "https://www.dnd5eapi.co/api/spells",
+            equipment: "https://www.dnd5eapi.co/api/equipment",
+            monsters: "https://www.dnd5eapi.co/api/monsters",
+            backgrounds: "https://www.dnd5eapi.co/api/backgrounds"
+        };
+
+        // Exemplo básico de como usar fetch para obter dados da API D&D 5e
+        fetch(categoryEndpoints[selectedCategory])
+            .then(response => response.json())
+            .then(data => {
+                // Atualize a seção de resultados com os dados recebidos da API
+                const resultsContainer = document.getElementById("results");
+                resultsContainer.innerHTML = "";
+
+                if (data.results && data.results.length > 0) {
+                    // Ordenar os resultados com base na opção de ordenação escolhida
+                    let sortedResults;
+                    if (sortOrder === "alphabetical_asc") {
+                        sortedResults = data.results.sort((a, b) => a.name.localeCompare(b.name));
+                    } else if (sortOrder === "alphabetical_desc") {
+                        sortedResults = data.results.sort((a, b) => b.name.localeCompare(a.name));
+                    }
+                    // Adicione mais lógica para outras opções de ordenação, se necessário
+                    // ...
+
+                    sortedResults.forEach(item => {
+                        const itemElement = document.createElement("div");
+                        itemElement.innerHTML = `<h3>${item.name}</h3>`;
+                        resultsContainer.appendChild(itemElement);
+                    });
+                } else {
+                    resultsContainer.innerHTML = "<p>Nenhum resultado encontrado.</p>";
+                }
+            })
+            .catch(error => console.error('Erro ao chamar a API D&D 5e:', error));
     });
 
-    // Initial load
-    searchFoxes();
+    // Adicione manipuladores de eventos para filtragem e ordenação aqui
 });
+v
